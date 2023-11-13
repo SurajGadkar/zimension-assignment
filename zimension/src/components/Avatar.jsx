82d0879bb39193052393eb4ae51b6511e2e0f61a
Suraj Gadkar <surajgadkar14@gmail.com>
2023-11-13 20:26:43 +0530

    Avatar problem Occurred: removed for now

--- zimension/src/components/Avatar.jsx
+++ zimension/src/components/Avatar.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import styles from "./Avatar.module.css";
 
-import { CgProfile } from "react-icons/cg";
-
 function Avatar() {
   return (
     <div className={styles.main__container}>
       <div className={styles.account__text}>
         <h4>Account</h4>
       </div>
-      <div className={styles.profile__container}>
-        <CgProfile size={24} />
-      </div>
+      <div className={styles.profile__container}>profile</div>
     </div>
   );
 }
